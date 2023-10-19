dc841a5d93fee378c1393387b8ec9e9be9acffff
Kitty Yuan <kitty.yuan@kindredgroup.com>
2023-10-19 21:51:33 +1100

    main add json files and config json file process path

--- config/project_config.py
+++ config/project_config.py
@@ -1,8 +1,11 @@
 import time
 
 # ######### -- Configure the log output path ######### #
-log_root_path = "../logs/"
+log_root_path = "/Users/kityua/PycharmProjects-gaoji/ETL-Python/logs/"
 
 # ######### --Configure the log output filename ######### #
 log_name = f'pyetl-{time.strftime("%Y-%m-%d %H", time.localtime(time.time()))}.log'
 # print(log_name)  # 2023-07-08 14:09:43
+
+# ######### --Configure with process JSON file ######### #
+json_data_root_path = "/Users/kityua/PycharmProjects-gaoji/ETL-Python/json"
--- json_service.py
+++ json_service.py
@@ -1,5 +1,13 @@
 from util.logging_util import init_logger
+from util import file_util as fu
+from config import project_config as conf
 
-# TODO: Step 1, identify which files need to be processed
+# Step 1, log process JSON file info
 logger = init_logger()
 logger.info("Processing JSON data, the program has started...")
+
+# identify which files need to be processed
+files = fu.get_dir_files_list(conf.json_data_root_path)
+print(files)
+logger.info(f"Inspecting the JSON folder, the following files are found: {files}")
+
