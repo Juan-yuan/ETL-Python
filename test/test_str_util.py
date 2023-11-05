0d5070389752757433043c33c99917cfebb725da
Kitty Yuan <kitty.yuan@kindredgroup.com>
2023-11-05 14:42:35 +1100

    feat: main add check_str_null_and_transform_to_sql_null function add tests

--- test/test_str_util.py
+++ test/test_str_util.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from util.str_util import check_null
+from util.str_util import check_null, check_str_null_and_transform_to_sql_null
 
 class TestStrUtil(TestCase):
     def setUp(self) -> None:
@@ -25,3 +25,24 @@ class TestStrUtil(TestCase):
         s = 'Not-null'
         result = check_null(s)
         self.assertEqual(False, result)
+
+    def check_check_str_null_and_transform_to_sql_null(self):
+        name = ""
+        result = check_str_null_and_transform_to_sql_null(name)
+        self.assertEqual("NULL", name)
+
+        name = "None"
+        result = check_str_null_and_transform_to_sql_null(name)
+        self.assertEqual("NULL", name)
+
+        name = "null"
+        result = check_str_null_and_transform_to_sql_null(name)
+        self.assertEqual("NULL", name)
+
+        name = "undefined"
+        result = check_str_null_and_transform_to_sql_null(name)
+        self.assertEqual("NULL", name)
+
+        name = 'John'
+        name = check_str_null_and_transform_to_sql_null(name)
+        self.assertEqual('John', name)
--- util/str_util.py
+++ util/str_util.py
@@ -5,3 +5,17 @@ def check_null(data):
     if data == "none" or data == "" or data == "null" or data == "undefined":
         return True
     return False
+
+
+def check_str_null_and_transform_to_sql_null(data):
+    if check_null(data):
+        return "NULL"
+    else:
+        return f"'{data}'"
+
+
+def check_number_null_and_transform_to_sql_null(data):
+    if data and not check_null(str(data)):
+        return data
+    else:
+        return "NULL"
