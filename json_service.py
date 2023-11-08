8aec74248a9c7427709b970bd2ebf7e879a7cec4
Kitty Yuan <kitty.yuan@kindredgroup.com>
2023-11-08 14:06:46 +1100

    feat: main insert data into order and details tables

--- json_service.py
+++ json_service.py
@@ -1,8 +1,14 @@
+import time
+
 from util.logging_util import init_logger
 from util import file_util as fu
 from config import project_config as conf
 from util.mysql_util import MySQLUtil, get_processed_files
 from model.retail_order_model import OrdersModel, OrderDetailModel
+
+# check the process time
+# print(time.strftime("%H:%M:%S", time.localtime(time.time())))
+# 13:59:30
 
 # Step 1, Handle files are need to process
 logger = init_logger()
@@ -76,6 +82,7 @@ for file in need_to_process_files:
 # logger.info(f"完成了csv备份文件的写出，写出到了: {conf.retail_output_csv_root_path}")
 
     # 2. Store orders and order details data to MySQL db
+    # create table
     target_db_util.check_table_exists_and_create(
         conf.target_db_name,
         conf.target_orders_table_name,
@@ -86,6 +93,23 @@ for file in need_to_process_files:
         conf.target_orders_detail_table_name,
         conf.target_orders_detail_table_create_cols
     )
+    # insert data
+    for model in reserved_models:
+        insert_sql = model.generate_insert_sql()
+        target_db_util.select_db(conf.target_db_name)
+        target_db_util.execute(insert_sql)
+    # insert order detail data
+    for model in order_detail_model_list:
+        insert_sql = model.generate_insert_sql()
+        target_db_util.select_db(conf.target_db_name)
+        target_db_util.execute(insert_sql)
+
+logger.info(f"Finished CSV back up，write to：{conf.retail_output_csv_root_path}")
+logger.info(f"Finished store data to MySQL "
+            f"Processed ：{len(reserved_models)} data")
+# check the process time
+# print(time.strftime("%H:%M:%S", time.localtime(time.time())))
+# 13:59:30
 
 
 
--- util/str_util.py
+++ util/str_util.py
@@ -1,9 +1,11 @@
 def check_null(data):
     if not data:
         return True
-    data = data.lower()
-    if data == "none" or data == "" or data == "null" or data == "undefined":
-        return True
+
+    if isinstance(data, str):
+        data = data.lower()
+        if data == "none" or data == "" or data == "null" or data == "undefined":
+            return True
     return False
 
 
