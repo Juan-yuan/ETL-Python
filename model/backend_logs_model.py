70fe0d86f544b7ce9893006f36dbd97acece7383
Kitty Yuan <kitty.yuan@kindredgroup.com>
2023-11-18 18:16:35 +1100

    feat: main add backend_logs_model model file

---
+++ model/backend_logs_model.py
@@ -0,0 +1,47 @@
+'''
+backend logs model
+'''
+
+from config import project_config as conf
+
+class BackendLogsModel:
+    def __init__(self, data: str, sep="\t"):    # \t: 是制表符，也就是 tab 键，按照 tab 键去拆分数据
+        arrs = data.split(sep)
+
+        self.log_time = arrs[0]
+        self.log_level = arrs[1].replace("[", "").replace("]", "") # 日志级别，处理方括号
+        self.log_module = arrs[2]
+        self.response_time = int(arrs[3][:-2][5:])
+        self.province = arrs[4]
+        self.city = arrs[5]
+        self.log_text = arrs[6]
+
+    def to_string(self):
+        return f"log_time: {self.log_time}, " \
+               f"log_level: {self.log_level}" \
+               f"log_module: {self.log_module}" \
+               f"response_time: {self.response_time}" \
+               f"province: {self.province}, " \
+               f"city: {self.city}, " \
+               f"log_text: {self.log_text}"
+
+    def generate_insert_sql(self):
+        return f"INSERT INTO {conf.target_backend_logs_store_table_name}(" \
+               f"log_time, log_level, log_module, response_time, province, city, log_text) VALUES(" \
+               f"'{self.log_time}', " \
+               f"'{self.log_level}', " \
+               f"'{self.log_module}', " \
+               f"{self.response_time}, " \
+               f"'{self.province}', " \
+               f"'{self.city}', " \
+               f"'{self.log_text}')"
+
+    def to_csv(self, sep=","):
+        return \
+            f"{self.log_time}{sep}" \
+            f"{self.log_level}{sep}" \
+            f"{self.log_module}{sep}" \
+            f"{self.response_time}{sep}" \
+            f"{self.province}{sep}" \
+            f"{self.city}{sep}" \
+            f"{self.log_text}"
